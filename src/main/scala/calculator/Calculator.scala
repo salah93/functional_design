84dfda45d597243f0016a896147d867e12b020ee
salah ahmed <salah93@crosscompute.com>
2018-07-23 15:53:07 -0400

    calculator project

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,38 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.keys.toList.map((variable_name) =>
+      variable_name -> Signal(eval(
+          namedExpressions.get(variable_name).head(),
+          namedExpressions - variable_name))).toMap
+  }
+
+  def ensureValid(a: Expr, b: Expr, references: Map[String, Signal[Expr]])
+                 (fn: (Double, Double) => Double): Double = {
+    val first  = eval(a, references)
+    val second = eval(b, references)
+    if (first == Double.NaN || second == Double.NaN)
+      Double.NaN
+    else
+      fn(first, second)
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v: Double) => v
+      case Ref(name: String) => {
+        val e = getReferenceExpr(name, references)
+        eval(e, references - name)
+      }
+      case Plus(a: Expr, b: Expr) =>
+        ensureValid(a, b, references)((a, b) => a + b)
+      case Minus(a: Expr, b: Expr) =>
+        ensureValid(a, b, references)((a, b) => a - b)
+      case Times(a: Expr, b: Expr) =>
+        ensureValid(a, b, references)((a, b) => a * b)
+      case Divide(a: Expr, b: Expr) =>
+        ensureValid(a, b, references)((a, b) => a / b)
+    }
   }
 
   /** Get the Expr for a referenced variables.
